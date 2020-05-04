600b0bb671fc39275e0d344587a9de8e552c6414
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-05-03 20:42:21 -0400

    Working on props and mapping, adding ReactStrap

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,13 +1,40 @@
 // Write your Character component here
-import React from 'react';
-import 
+import React, { useState, useEffect } from 'react';
+import CharacterCard from "./CharacterCard"
+import axios from "axios";
 
 function Character() {
+
+    const [individual, setIndividual] = useState([]);
+
+    useEffect(() => {
+        axios
+          .get("https://swapi.py4e.com/api/people/1/")
+          .then(response => {
+            console.log(response.data);
+            setIndividual(response.data);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }, []);
+
     return (
-        <div>
+        <div className="character-profile">
+            {individual.map(info => {
+                return (
+                    <CharacterCard 
+                    name={info.name}
+                    height={info.height}
+                    birth_year={info.birth_year}
+                    gender={info.gender}
+                    homeworld={info.homeworld}
+                    />
+                );
+            })}
 
         </div>
-    )
+    );
 }
 
 export default Character;
